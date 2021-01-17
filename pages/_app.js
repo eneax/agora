b730df259ac7d05f6f53e170b34aed731317dcf1
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-01-17 17:48:13 +0100

    Config Chakra UI and Absolute Imports (#4)
    * Get started with chakra-ui
    
    * Use absolute imports and module path aliases
    
    * Add GlobalStyles
    
    * Remove unused code

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
-import { AuthProvider } from '../lib/auth';
-import '../styles/globals.css';
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme';
+import GlobalStyle from '@/styles/globalStyles';
 
 const App = ({ Component, pageProps }) => (
-  <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>
+  <ChakraProvider resetCSS theme={customTheme}>
+    <AuthProvider>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </AuthProvider>
+  </ChakraProvider>
 );
 
 export default App;
