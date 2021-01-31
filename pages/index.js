b7af1c496841fa5d62f1362777b972a12bf749f5
eneax <19818937+eneax@users.noreply.github.com>
2021-01-31 15:21:08 +0100

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
-import { Button, Icon, Flex } from '@chakra-ui/react';
+import { Button, Icon, Flex, Text } from '@chakra-ui/react';
 import { MdGroupWork } from 'react-icons/md';
 
 import { useAuth } from '@/lib/auth';
+import Link from 'next/link';
 
 const Home = () => {
   const auth = useAuth();
@@ -14,6 +15,8 @@ const Home = () => {
       align="center"
       justify="center"
       h="100vh"
+      maxW="600px"
+      margin="0 auto"
     >
       <Head>
         <script
@@ -30,13 +33,25 @@ const Home = () => {
 
       <Icon as={MdGroupWork} name="logo" color="black" w={10} h={10} />
 
+      <Text my={4}>
+        <Text as="span" fontWeight="bold" display="inline">
+          Agora
+        </Text>{' '}
+        is the easiest way to add comments or reviews to your static site. It's
+        still a work-in-progress, but you can try it out by logging in.
+      </Text>
       {auth.user ? (
-        <Button as="a" href="/dashboard" mt={4}>
+        <Button as="a" size="sm" fontWeight="medium" href="/dashboard">
           View Dashboard
         </Button>
       ) : (
-        <Button size="sm" mt={4} onClick={() => auth.signinWithGitHub()}>
-          Sign in with GitHub
+        <Button
+          mt={4}
+          size="sm"
+          fontWeight="medium"
+          onClick={() => auth.signinWithGitHub()}
+        >
+          Sign In
         </Button>
       )}
     </Flex>
